4656a223517006f12ed3a203b6bc44e40a77453e
Omar <3omarthu@gmail.com>
2021-02-05 20:35:37 +0300

    update all the requaired

--- FSND-master/projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ FSND-master/projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -1,13 +1,13 @@
 import json
-from flask import Flask, request, abort, jsonify, _request_ctx_stack
+from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
-from urllib.request import urlopen
+from urllib2 import urlopen
 
 
 AUTH0_DOMAIN = 'fsndomar.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'coffee-shop'
+API_AUDIENCE = 'https://coffee-shop.com/'
 
 # AuthError Exception
 '''
@@ -36,15 +36,20 @@ class AuthError(Exception):
 
 def get_token_auth_header():
     if 'Authorization' not in request.headers:
-        abort(404)
+        abort(401)
 
     auth_header = request.headers['Authorization']
-    header_parts = auth_header.split(' ')
+    if not auth_header:
+        raise AuthError({"code": "authorization_header_missing",
+                        "description": "Authorization header is expected"}, 401)
+    header_parts = auth_header.split()
 
     if len(header_parts) != 2:
-        abort(401)
+        raise AuthError({"code": "invalid_header",
+                        "description": "Authorization header is malformed"}, 401)
     elif header_parts[0].lower() != 'bearer':
-        abort(401)
+        raise AuthError({"code": "invalid_header",
+                        "description": "Authorization header must start with" " Bearer"}, 401)
     return header_parts[1]
 
 '''
@@ -92,7 +97,7 @@ def check_permissions(permission, payload):
 
 
 def verify_decode_jwt(token):
-    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
     rsa_key = {}
@@ -140,10 +145,7 @@ def verify_decode_jwt(token):
                 'code': 'invalid_header',
                 'description': 'Unable to parse authentication token.'
             }, 400)
-    raise AuthError({
-                'code': 'invalid_header',
-                'description': 'Unable to find the appropriate key.'
-            }, 400)
+    
 
 '''
 @Done implement @requires_auth(permission) decorator method
@@ -163,14 +165,12 @@ def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
-            token = get_token_auth_header()
             try:
+                token = get_token_auth_header()
                 payload = verify_decode_jwt(token)
+                check_permissions(permission, payload)
+                return f(payload, *args, **kwargs)
             except:
                 abort(401)
-
-            check_permissions(permission, payload)
-            return f(payload, *args, **kwargs)
-
         return wrapper
     return requires_auth_decorator
